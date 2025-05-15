27911c70c429717b8850dc04d3e00eeb2c6e4634
LinHeMa <linhema0921@gmail.com>
2025-05-15 15:46:14 +0800

    feat: add live section component and integrate into homepage

--- graphql/__generated__/graphql.ts
+++ graphql/__generated__/graphql.ts
@@ -2325,6 +2325,8 @@ export type PopularTagWhereUniqueInput = {
 export type Post = {
   __typename?: 'Post'
   Warning?: Maybe<Warning>
+  Warnings?: Maybe<Array<Warning>>
+  WarningsCount?: Maybe<Scalars['Int']['output']>
   adTrace?: Maybe<Scalars['String']['output']>
   apiData?: Maybe<Scalars['JSON']['output']>
   apiDataBrief?: Maybe<Scalars['JSON']['output']>
@@ -2402,6 +2404,18 @@ export type Post = {
   writers?: Maybe<Array<Contact>>
   writersCount?: Maybe<Scalars['Int']['output']>
   writersInInputOrder?: Maybe<Array<Maybe<Contact>>>
+}
+
+export type PostWarningsArgs = {
+  cursor?: InputMaybe<WarningWhereUniqueInput>
+  orderBy?: Array<WarningOrderByInput>
+  skip?: Scalars['Int']['input']
+  take?: InputMaybe<Scalars['Int']['input']>
+  where?: WarningWhereInput
+}
+
+export type PostWarningsCountArgs = {
+  where?: WarningWhereInput
 }
 
 export type PostCamera_ManArgs = {
@@ -2574,6 +2588,7 @@ export type PostWritersCountArgs = {
 
 export type PostCreateInput = {
   Warning?: InputMaybe<WarningRelateToOneForCreateInput>
+  Warnings?: InputMaybe<WarningRelateToManyForCreateInput>
   adTrace?: InputMaybe<Scalars['String']['input']>
   apiData?: InputMaybe<Scalars['JSON']['input']>
   apiDataBrief?: InputMaybe<Scalars['JSON']['input']>
@@ -2692,6 +2707,7 @@ export type PostUpdateArgs = {
 
 export type PostUpdateInput = {
   Warning?: InputMaybe<WarningRelateToOneForUpdateInput>
+  Warnings?: InputMaybe<WarningRelateToManyForUpdateInput>
   adTrace?: InputMaybe<Scalars['String']['input']>
   apiData?: InputMaybe<Scalars['JSON']['input']>
   apiDataBrief?: InputMaybe<Scalars['JSON']['input']>
@@ -2753,6 +2769,7 @@ export type PostWhereInput = {
   NOT?: InputMaybe<Array<PostWhereInput>>
   OR?: InputMaybe<Array<PostWhereInput>>
   Warning?: InputMaybe<WarningWhereInput>
+  Warnings?: InputMaybe<WarningManyRelationFilter>
   adTrace?: InputMaybe<StringFilter>
   camera_man?: InputMaybe<ContactManyRelationFilter>
   categories?: InputMaybe<CategoryManyRelationFilter>
@@ -3959,12 +3976,14 @@ export type Video = {
   manualOrderOfRelatedPosts?: Maybe<Scalars['JSON']['output']>
   name?: Maybe<Scalars['String']['output']>
   publishedDate?: Maybe<Scalars['DateTime']['output']>
+  publishedDateString?: Maybe<Scalars['String']['output']>
   related_posts?: Maybe<Array<Post>>
   related_postsCount?: Maybe<Scalars['Int']['output']>
   related_postsInInputOrder?: Maybe<Array<Maybe<Post>>>
   state?: Maybe<Scalars['String']['output']>
   tags?: Maybe<Array<Tag>>
   tagsCount?: Maybe<Scalars['Int']['output']>
+  updateTimeStamp?: Maybe<Scalars['Boolean']['output']>
   updatedAt?: Maybe<Scalars['DateTime']['output']>
   updatedBy?: Maybe<User>
   uploader?: Maybe<Scalars['String']['output']>
@@ -4009,9 +4028,11 @@ export type VideoCreateInput = {
   manualOrderOfRelatedPosts?: InputMaybe<Scalars['JSON']['input']>
   name?: InputMaybe<Scalars['String']['input']>
   publishedDate?: InputMaybe<Scalars['DateTime']['input']>
+  publishedDateString?: InputMaybe<Scalars['String']['input']>
   related_posts?: InputMaybe<PostRelateToManyForCreateInput>
   state?: InputMaybe<Scalars['String']['input']>
   tags?: InputMaybe<TagRelateToManyForCreateInput>
+  updateTimeStamp?: InputMaybe<Scalars['Boolean']['input']>
   updatedAt?: InputMaybe<Scalars['DateTime']['input']>
   updatedBy?: InputMaybe<UserRelateToOneForCreateInput>
   uploader?: InputMaybe<Scalars['String']['input']>
@@ -4028,7 +4049,9 @@ export type VideoOrderByInput = {
   isShorts?: InputMaybe<OrderDirection>
   name?: InputMaybe<OrderDirection>
   publishedDate?: InputMaybe<OrderDirection>
+  publishedDateString?: InputMaybe<OrderDirection>
   state?: InputMaybe<OrderDirection>
+  updateTimeStamp?: InputMaybe<OrderDirection>
   updatedAt?: InputMaybe<OrderDirection>
   uploader?: InputMaybe<OrderDirection>
   uploaderEmail?: InputMaybe<OrderDirection>
@@ -4075,9 +4098,11 @@ export type VideoUpdateInput = {
   manualOrderOfRelatedPosts?: InputMaybe<Scalars['JSON']['input']>
   name?: InputMaybe<Scalars['String']['input']>
   publishedDate?: InputMaybe<Scalars['DateTime']['input']>
+  publishedDateString?: InputMaybe<Scalars['String']['input']>
   related_posts?: InputMaybe<PostRelateToManyForUpdateInput>
   state?: InputMaybe<Scalars['String']['input']>
   tags?: InputMaybe<TagRelateToManyForUpdateInput>
+  updateTimeStamp?: InputMaybe<Scalars['Boolean']['input']>
   updatedAt?: InputMaybe<Scalars['DateTime']['input']>
   updatedBy?: InputMaybe<UserRelateToOneForUpdateInput>
   uploader?: InputMaybe<Scalars['String']['input']>
@@ -4099,6 +4124,7 @@ export type VideoWhereInput = {
   isShorts?: InputMaybe<BooleanFilter>
   name?: InputMaybe<StringFilter>
   publishedDate?: InputMaybe<DateTimeNullableFilter>
+  publishedDateString?: InputMaybe<StringFilter>
   related_posts?: InputMaybe<PostManyRelationFilter>
   state?: InputMaybe<StringNullableFilter>
   tags?: InputMaybe<TagManyRelationFilter>
@@ -4132,11 +4158,29 @@ export type WarningCreateInput = {
   updatedBy?: InputMaybe<UserRelateToOneForCreateInput>
 }
 
+export type WarningManyRelationFilter = {
+  every?: InputMaybe<WarningWhereInput>
+  none?: InputMaybe<WarningWhereInput>
+  some?: InputMaybe<WarningWhereInput>
+}
+
 export type WarningOrderByInput = {
   content?: InputMaybe<OrderDirection>
   createdAt?: InputMaybe<OrderDirection>
   id?: InputMaybe<OrderDirection>
   updatedAt?: InputMaybe<OrderDirection>
+}
+
+export type WarningRelateToManyForCreateInput = {
+  connect?: InputMaybe<Array<WarningWhereUniqueInput>>
+  create?: InputMaybe<Array<WarningCreateInput>>
+}
+
+export type WarningRelateToManyForUpdateInput = {
+  connect?: InputMaybe<Array<WarningWhereUniqueInput>>
+  create?: InputMaybe<Array<WarningCreateInput>>
+  disconnect?: InputMaybe<Array<WarningWhereUniqueInput>>
+  set?: InputMaybe<Array<WarningWhereUniqueInput>>
 }
 
 export type WarningRelateToOneForCreateInput = {
